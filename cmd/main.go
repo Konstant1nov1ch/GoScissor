527f112ef059d01b2154964684a7d6b02a7ba748
konstantin <Kostya.pinchuk.04@gmail.com>
2023-05-11 00:05:31 +0300

    [+] front-client js

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
+	"net/http"
 	"time"
 )
 
@@ -19,25 +20,36 @@ func main() {
 	defer func(db *gorm.DB) {
 		err := db.Close()
 		if err != nil {
-
+			log.Println(err)
 		}
 	}(db)
 
 	db.AutoMigrate(&Token{})
 
-	cache := cache.NewCache(10, 100, time.Hour*24)
+	cache := cache.NewCache(10, 100, time.Minute*10)
 
 	router := gin.Default()
 	// указываем директорию с шаблонами HTML
 	router.LoadHTMLGlob("web/templates/*.html")
 
-	// определяем маршрут для /admin/tokens
+	router.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "POST, GET")
+		c.Header("Access-Control-Allow-Headers", "Content-Type")
+	})
+
+	router.OPTIONS("/sci", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+
 	router.GET("/admin/tokens", Admin(db))
-	router.GET(":short_url", Redirect(db, cache))
+	router.GET("/:short_url", Redirect(db, cache))
+
+	// Добавляем обработчик для запросов POST на путь /sci
 	router.POST("/sci", CreateToken(db, cache))
 
-	err1 := router.Run(":8080")
-	if err1 != nil {
-		return
+	err = router.Run(":8080")
+	if err != nil {
+		log.Println(err)
 	}
 }
